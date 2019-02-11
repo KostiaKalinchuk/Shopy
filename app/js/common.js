16f132c4c55055880652574b079a5b0ad1a8a46f
Your Name <9leon1@gmail.com>
2019-02-11 23:12:20 +0200

    fixed adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -73,3 +73,16 @@ function catalogItemCounter(field){
 catalogItemCounter('.fieldCount');
 
 
+
+$(function(){
+
+    $('.menuToggle').on('click', function() {
+        $('.menu').slideToggle(300, function(){
+            if( $(this).css('display') === "none"){
+                $(this).removeAttr('style');
+            }
+        });
+
+    });
+
+});
